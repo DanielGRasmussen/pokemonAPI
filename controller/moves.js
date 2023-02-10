f42e9591ce170499604ece00b38b947f5f996282
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-02-10 09:45:32 -0700

    Added error handling.

--- controller/moves.js
+++ controller/moves.js
@@ -1,92 +1,108 @@
 const mongo = require("../mongo");
 
 exports.getMove = function (req, res) {
-	const move = req.params.move;
-	mongo.getByName("moves", move).then((move) => {
-		if (move) {
-			res.setHeader("Content-Type", "application/json");
-			res.status(200).json(move);
-		} else {
-			res.status(404).send("Move not found.");
-		}
-	});
+	try {
+		const move = req.params.move;
+		mongo.getByName("moves", move).then((move) => {
+			if (move) {
+				res.setHeader("Content-Type", "application/json");
+				res.status(200).json(move);
+			} else {
+				res.status(404).send("Move not found.");
+			}
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
 
 exports.addMove = function (req, res) {
-	const doc = {
-		name: req.body.name,
-		type: req.body.type,
-		power: req.body.power,
-		accuracy: req.body.accuracy
-	};
+	try {
+		const doc = {
+			name: req.body.name,
+			type: req.body.type,
+			power: req.body.power,
+			accuracy: req.body.accuracy
+		};
 
-	const allFieldsExist = doc.name && doc.type && doc.power && doc.accuracy;
+		const allFieldsExist = doc.name && doc.type && doc.power && doc.accuracy;
 
-	if (allFieldsExist) {
-		mongo.post("moves", doc).then((response) => {
-			if (response.acknowledged) {
-				res.status(201).json(response.insertedId);
-			} else {
-				res.status(500).json(
-					response.error || "Some error occurred while creating the move."
-				);
-			}
-		});
-	} else {
-		res.status(400).json("All fields must be filled out.");
+		if (allFieldsExist) {
+			mongo.post("moves", doc).then((response) => {
+				if (response.acknowledged) {
+					res.status(201).json(response.insertedId);
+				} else {
+					res.status(500).json(
+						response.error || "Some error occurred while creating the move."
+					);
+				}
+			});
+		} else {
+			res.status(400).json("All fields must be filled out.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
 
 exports.updateMove = function (req, res) {
-	const id = req.params.id;
-	const doc = {
-		name: req.body.name,
-		type: req.body.type,
-		power: req.body.power,
-		accuracy: req.body.accuracy
-	};
+	try {
+		const id = req.params.id;
+		const doc = {
+			name: req.body.name,
+			type: req.body.type,
+			power: req.body.power,
+			accuracy: req.body.accuracy
+		};
 
-	const allFieldsExist = doc.name && doc.type && doc.power && doc.accuracy;
-	const validId = id.length === 24;
+		const allFieldsExist = doc.name && doc.type && doc.power && doc.accuracy;
+		const validId = id.length === 24;
 
-	if (allFieldsExist && validId) {
-		mongo.put("moves", id, doc).then((response) => {
-			if (response.modifiedCount > 0) {
-				res.status(204).send();
-			} else {
-				if (!response.length) {
-					res.status(404).json(response.error || "Move was not found.");
+		if (allFieldsExist && validId) {
+			mongo.put("moves", id, doc).then((response) => {
+				if (response.modifiedCount > 0) {
+					res.status(204).send();
 				} else {
-					res.status(500).json(
-						response.error || "Some error occurred while updating the move."
-					);
+					if (!response.length) {
+						res.status(404).json(response.error || "Move was not found.");
+					} else {
+						res.status(500).json(
+							response.error || "Some error occurred while updating the move."
+						);
+					}
 				}
-			}
-		});
-	} else if (validId) {
-		res.status(400).json("All fields must be filled out.");
-	} else {
-		res.status(400).json("Invalid ID.");
+			});
+		} else if (validId) {
+			res.status(400).json("All fields must be filled out.");
+		} else {
+			res.status(400).json("Invalid ID.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
 
 exports.deleteMove = function (req, res) {
-	const id = req.params.id;
+	try {
+		const id = req.params.id;
 
-	if (id.length === 24) {
-		mongo.delete("moves", id).then((response) => {
-			if (response.deletedCount > 0) {
-				res.status(204).send();
-			} else {
-				if (!response.length) {
-					res.status(404).json(response.error || "Move was not found.");
+		if (id.length === 24) {
+			mongo.delete("moves", id).then((response) => {
+				if (response.deletedCount > 0) {
+					res.status(200).json("Deleted the Pokemon.");
+				} else {
+					if (!response.length) {
+						res.status(404).json(response.error || "Move was not found.");
+					}
+					res.status(500).json(
+						response.error || "Some error occurred while deleting the move."
+					);
 				}
-				res.status(500).json(
-					response.error || "Some error occurred while deleting the move."
-				);
-			}
-		});
-	} else {
-		res.status(400).json("Invalid ID.");
+			});
+		} else {
+			res.status(400).json("Invalid ID.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
--- controller/pokemon.js
+++ controller/pokemon.js
@@ -1,102 +1,128 @@
 const mongo = require("../mongo");
 
 exports.getPokemon = function (req, res) {
-	const pokemon = req.params.pokemon;
-	mongo.getByName("pokemon", pokemon).then((pokemon) => {
-		if (pokemon) {
-			res.setHeader("Content-Type", "application/json");
-			res.status(200).json(pokemon);
-		} else {
-			res.status(404).json("Pokemon not found.");
-		}
-	});
+	try {
+		const pokemon = req.params.pokemon;
+		mongo.getByName("pokemon", pokemon).then((pokemon) => {
+			if (pokemon) {
+				res.setHeader("Content-Type", "application/json");
+				res.status(200).json(pokemon);
+			} else {
+				res.status(404).json("Pokemon not found.");
+			}
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
 
 exports.addPokemon = function (req, res) {
-	const doc = {
-		name: req.body.name,
-		types: req.body.types,
-		generation: req.body.generation,
-		pokedex_number: req.body.pokedex_number,
-		height: req.body.height,
-		weight: req.body.weight,
-		evolve_from: req.body.evolve_from,
-		evolve_to: req.body.evolve_to
-	};
+	try {
+		const doc = {
+			name: req.body.name,
+			types: req.body.types,
+			generation: req.body.generation,
+			pokedex_number: req.body.pokedex_number,
+			height: req.body.height,
+			weight: req.body.weight,
+			evolve_from: req.body.evolve_from,
+			evolve_to: req.body.evolve_to
+		};
 
-	const allFieldsExist =
-		doc.name && doc.types && doc.generation && doc.pokedex_number && doc.height && doc.weight;
+		const allFieldsExist =
+			doc.name &&
+			doc.types &&
+			doc.generation &&
+			doc.pokedex_number &&
+			doc.height &&
+			doc.weight;
 
-	if (allFieldsExist) {
-		mongo.post("pokemon", doc).then((response) => {
-			if (response.acknowledged) {
-				res.status(201).json(response.insertedId);
-			} else {
-				res.status(500).json(
-					response.error || "Some error occurred while creating the Pokemon."
-				);
-			}
-		});
-	} else {
-		res.status(400).json("All fields must be filled out.");
+		if (allFieldsExist) {
+			mongo.post("pokemon", doc).then((response) => {
+				if (response.acknowledged) {
+					res.status(201).json(response.insertedId);
+				} else {
+					res.status(500).json(
+						response.error || "Some error occurred while creating the Pokemon."
+					);
+				}
+			});
+		} else {
+			res.status(400).json("All fields must be filled out.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
 
 exports.updatePokemon = function (req, res) {
-	const id = req.params.id;
-	const doc = {
-		name: req.body.name,
-		types: req.body.types,
-		generation: req.body.generation,
-		pokedex_number: req.body.pokedex_number,
-		height: req.body.height,
-		weight: req.body.weight,
-		evolve_from: req.body.evolve_from,
-		evolve_to: req.body.evolve_to
-	};
+	try {
+		const id = req.params.id;
+		const doc = {
+			name: req.body.name,
+			types: req.body.types,
+			generation: req.body.generation,
+			pokedex_number: req.body.pokedex_number,
+			height: req.body.height,
+			weight: req.body.weight,
+			evolve_from: req.body.evolve_from,
+			evolve_to: req.body.evolve_to
+		};
 
-	const allFieldsExist =
-		doc.name && doc.types && doc.generation && doc.pokedex_number && doc.height && doc.weight;
-	const validId = id.length === 24;
+		const allFieldsExist =
+			doc.name &&
+			doc.types &&
+			doc.generation &&
+			doc.pokedex_number &&
+			doc.height &&
+			doc.weight;
+		const validId = id.length === 24;
 
-	if (allFieldsExist && validId) {
-		mongo.put("pokemon", id, doc).then((response) => {
-			if (response.modifiedCount > 0) {
-				res.status(204).send();
-			} else {
-				if (!response.length) {
-					res.status(404).json(response.error || "Pokemon was not found.");
+		if (allFieldsExist && validId) {
+			mongo.put("pokemon", id, doc).then((response) => {
+				if (response.modifiedCount > 0) {
+					res.status(204).send();
 				} else {
-					res.status(500).json(
-						response.error || "Some error occurred while updating the Pokemon."
-					);
+					if (!response.length) {
+						res.status(404).json(response.error || "Pokemon was not found.");
+					} else {
+						res.status(500).json(
+							response.error || "Some error occurred while updating the Pokemon."
+						);
+					}
 				}
-			}
-		});
-	} else if (validId) {
-		res.status(400).json("All fields must be filled out.");
-	} else {
-		res.status(400).json("Invalid ID.");
+			});
+		} else if (validId) {
+			res.status(400).json("All fields must be filled out.");
+		} else {
+			res.status(400).json("Invalid ID.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
 
 exports.deletePokemon = function (req, res) {
-	const id = req.params.id;
+	try {
+		const id = req.params.id;
 
-	if (id.length === 24) {
-		mongo.delete("pokemon", id).then((response) => {
-			if (response.deletedCount > 0) {
-				res.status(204).send();
-			} else {
-				if (!response.length) {
-					res.status(404).json(response.error || "Pokemon was not found.");
+		if (id.length === 24) {
+			mongo.delete("pokemon", id).then((response) => {
+				if (response.deletedCount > 0) {
+					res.status(200).json("Deleted the Pokemon.");
+				} else {
+					if (!response.length) {
+						res.status(404).json(response.error || "Pokemon was not found.");
+					}
+					res.status(500).json(
+						response.error || "Some error occurred while deleting the Pokemon."
+					);
 				}
-				res.status(500).json(
-					response.error || "Some error occurred while deleting the Pokemon."
-				);
-			}
-		});
-	} else {
-		res.status(400).json("Invalid ID.");
+			});
+		} else {
+			res.status(400).json("Invalid ID.");
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
 };
